0f4ea8d89d2d683cb66998edb312c266ed3ca479
Krokodilushka <webmaster-krokodil@protonmail.com>
2024-01-10 14:14:02 +0500

    ...

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.QueueTask = void 0;
+var QueueTask_1 = require("./model/tasks/QueueTask");
+Object.defineProperty(exports, "QueueTask", { enumerable: true, get: function () { return QueueTask_1.QueueTask; } });
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {WebSocket} from './model/WebSocket'
+export {ServerApi} from './model/ServerApi'
+export {QueueTask} from './model/tasks/QueueTask'
