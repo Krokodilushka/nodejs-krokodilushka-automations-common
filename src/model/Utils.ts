f3098090aa4a61f49027575c96b08f95e1a5931b
Krokodilushka <webmaster-krokodil@protonmail.com>
2024-02-16 13:20:38 +0500

    Прокси regex

--- src/model/Utils.ts
+++ src/model/Utils.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
-export const proxyRegex = /^(.*):(.*)@(.*)(.*)$/
+export const proxyRegex = /^(.+):(.+)@(.+):(.+)$/
 export const cookieZod = z.object({
     name: z.string(),
     value: z.string(),
